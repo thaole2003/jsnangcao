04b7ebe00ed335a44cbafbd885dfe8a89455a7ab
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-01-10 23:42:12 +0700

    toán tử spread

--- s9/script.js
+++ s9/script.js
@@ -30,58 +30,89 @@ const restauraut = {
             this.mainMenu[mainIndex] + "lúc" + time
         );
     },
+    oderPasta: function(ing1, ing2, ing3) {
+        console.log(`here is ${ing1},${ing2},${ing3}`);
+    },
 };
-const today = new Date();
-const time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//Destructuring_Objects
-const { name, openingHour, categories } = restauraut;
-const currentHours = restauraut.orderDelivery({
-    mainDeli: "gà chiên",
-    time: time,
-    address: "322 My dinh",
-    mainIndex: 1,
-    startMenu: 1,
-});
-// console.log(name, openingHour, categories);
-const {
-    name: nameRestaurant,
-    openingHour: hours,
-    categories: tags,
-} = restauraut;
-// console.log(nameRestaurant, hours, tags);
-const { menu = [], startMenu: starter = [] } = restauraut;
-// console.log(menu, starter);
-//mutating variables
-let ab = 1;
-let bc = 2;
-const objj = { ab: 3, bc: 4, ca: 5 };
-({ ab, bc } = objj);
-// console.log(ab, bc);
-//nested objects
-const {
-    fri: { open: op, close: cl },
-} = openingHour;
-// console.log(op, cl);
-
-//destructuring_array
+// toán tử spread Toán tử spread giải nén các phần tử trong một mảng
 const arr = [1, 2, 3];
-// console.log(arr);
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
-const [x, y, z] = arr;
-// console.log(x, y);
-let [first, second] = restauraut.categories;
-// console.log(first, second);
-// const three = first;
-// first = second;
-// second = three;
-// console.log(first, second);
-[first, second] = [second, first];
-// console.log(first, second);
-// console.log(restauraut.order(0, 1));
-const funoffun = [1, 2, [3, 4], 5];
-// console.log(funoffun[2][0]);
-const [p = 1, q = 1, r = 1] = [8, 1, 2];
-// console.log(p, q, r);
+const newArr = [...arr, 4, 5];
+// console.log(...newArr);
+//coppy array
+const arrCoppy = [...restauraut.startMenu];
+// console.log(arrCoppy);
+//join array
+const menu = [...restauraut.startMenu, ...restauraut.mainMenu];
+// console.log(menu);
+//iterables:array,string ,maps,set not object
+const str = "JONHNY";
+const letter = [...str, "", "E"];
+// console.log(letter);
+// const ingredient = [
+//     prompt("Let's is 1 "),
+//     prompt("Let's is 2 "),
+//     prompt("Let's is 3 "),
+// ];
+// restauraut.oderPasta((ingredient[0], ingredient[1], ingredient[2]));
+// restauraut.oderPasta(...ingredient);
+const restaurautNew = { fonderIn: 1998, ...restauraut, founder: "Guiders" };
+console.log(restaurautNew);
+const restaurautCoppy = {...restauraut };
+console.log(restaurautCoppy);
+restaurautCoppy.name = "abcdef";
+console.log(restaurautCoppy.name);
+console.log(restauraut.name);
+// const today = new Date();
+// const time =
+//     today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// //Destructuring_Objects
+// const { name, openingHour, categories } = restauraut;
+// const currentHours = restauraut.orderDelivery({
+//     mainDeli: "gà chiên",
+//     time: time,
+//     address: "322 My dinh",
+//     mainIndex: 1,
+//     startMenu: 1,
+// });
+// // console.log(name, openingHour, categories);
+// const {
+//     name: nameRestaurant,
+//     openingHour: hours,
+//     categories: tags,
+// } = restauraut;
+// // console.log(nameRestaurant, hours, tags);
+// const { menu = [], startMenu: starter = [] } = restauraut;
+// // console.log(menu, starter);
+// //mutating variables
+// let ab = 1;
+// let bc = 2;
+// const objj = { ab: 3, bc: 4, ca: 5 };
+// ({ ab, bc } = objj);
+// // console.log(ab, bc);
+// //nested objects
+// const {
+//     fri: { open: op, close: cl },
+// } = openingHour;
+// // console.log(op, cl);
+
+// //destructuring_array
+// const arr = [1, 2, 3];
+// // console.log(arr);
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+// const [x, y, z] = arr;
+// // console.log(x, y);
+// let [first, second] = restauraut.categories;
+// // console.log(first, second);
+// // const three = first;
+// // first = second;
+// // second = three;
+// // console.log(first, second);
+// [first, second] = [second, first];
+// // console.log(first, second);
+// // console.log(restauraut.order(0, 1));
+// const funoffun = [1, 2, [3, 4], 5];
+// // console.log(funoffun[2][0]);
+// const [p = 1, q = 1, r = 1] = [8, 1, 2];
+// // console.log(p, q, r);
