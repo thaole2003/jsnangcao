c781fafab00b11cf4a1302301758a920ab1d5337
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-01-10 19:46:31 +0700

    update s9 #2

--- s9/script.js
+++ s9/script.js
@@ -4,10 +4,29 @@ const restauraut = {
     categories: ["A", "B", "C"],
     startMenu: ["Ga", "melon"],
     mainMenu: ["Piza", "Fish"],
+    openingHour: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 0,
+            close: 24,
+        },
+        sat: {
+            open: 2,
+            close: 20,
+        },
+    },
     order: function(startIndex, mainIndex) {
         return [this.startMenu[startIndex], this.mainMenu[mainIndex]];
     },
 };
+//Destructuring_Objects
+const { name, openingHour, categories } = restauraut;
+console.log(name, openingHour, categories);
+
+//destructuring_array
 const arr = [1, 2, 3];
 // console.log(arr);
 const a = arr[0];
@@ -25,6 +44,6 @@ let [first, second] = restauraut.categories;
 // console.log(first, second);
 // console.log(restauraut.order(0, 1));
 const funoffun = [1, 2, [3, 4], 5];
-console.log(funoffun[2][0]);
+// console.log(funoffun[2][0]);
 const [p = 1, q = 1, r = 1] = [8, 1, 2];
-console.log(p, q, r);
+// console.log(p, q, r);
