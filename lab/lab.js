a0e14a5e7a3478239e91f9cc60bcb1d220ede988
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-01-12 14:36:07 +0700

    lab2

--- lab/lab.js
+++ lab/lab.js
@@ -33,11 +33,13 @@ const game = {
     scored: ["Marquinhos", "Bremer", "Ricardo Horta", "Andre Silva"],
     date: "Nov 9th 2037",
     odds: {
-        team1: 7.33,
+        team1: 1.33,
         x: 3.25,
         team2: 6.5,
     },
 };
+
+//lab1:Coding Challenge #1
 //1
 const [player1, player2] = game.player;
 // console.log(player1, player2);
@@ -62,3 +64,30 @@ printGoals(...game.scored);
 //7
 team1 < team2 && console.log("Team1 thắng cao hơn");
 team1 > team2 && console.log("Team2 thắng cao hơn");
+//lab2:Coding Challenge #2
+//1
+// const Goalscored = Object.entries(game.scored);
+for (const [x, y] of game.scored.entries()) {
+    console.log(`Goal ${x + 1}: ${y}`);
+}
+//2
+const Odds = Object.values(game.odds);
+console.log(Odds);
+let Avg = 0;
+for (let item of Odds) {
+    Avg += item;
+    const tbc = Avg / Odds.length;
+    console.log(tbc);
+}
+//3
+const oddss = Object.entries(game.odds);
+for (const [x, y] of oddss) {
+    const StrTeam = x === "x" ? "draw" : `victory ${game[x]}`;
+    console.log(`Odd of  ${StrTeam} is ${y}`);
+}
+//3.1
+const scorers = Object.values(game.scored);
+constsr  = {}
+for (let [x, y] of scorers) {
+    
+}
