d966a357609eeba15b125a67100c571a91a24dc5
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-01-31 17:28:45 +0700

    //143:The new at Method

--- s11/script.js
+++ s11/script.js
@@ -9,11 +9,19 @@
 // console.log(arr.splice(2));
 // arr.splice(-1);
 // console.log(arr);
-let arr = ["a", "b", "c", "d", "e", "f"];
-let arr2 = ["i", "j", "o", "r"];
-console.log(arr2.reverse());
+// let arr = ["a", "b", "c", "d", "e", "f"];
+// let arr2 = ["i", "j", "o", "r"];
+// console.log(arr2.reverse());
 // reverseđảo ngược trật tự
 //concat nối mảng
 //joinPhương thức array join() trong Javascript kết nối tất cả các phần tử trong một mảng thành một chuỗi.
-console.log(arr.concat(arr2));
-console.log(arr.join("-"));
+// console.log(arr.concat(arr2));
+// console.log(arr.join("-"));
+//143:The new at Method
+const arr = [12, 122, 33];
+console.log(arr[0]);
+console.log(arr.at(0));
+//get last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice((-1)[0]));
+console.log(arr.at(-1));
