da95e0bee39e4e331d2828aa2e7af6ae8e92cd07
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-01-31 17:23:27 +0700

    142. Simple Array Methods

---
+++ s11/script.js
@@ -0,0 +1,19 @@
+// let arr = ["a", "b", "c", "d", "e", "f"];
+// //slice
+// // console.log(arr.slice(2, 3));
+// // console.log(arr.slice(-2));
+// // console.log(arr.slice(1, -2));
+// console.log(arr.slice(2));
+// // console.log(...arr);
+// //splice
+// console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+let arr = ["a", "b", "c", "d", "e", "f"];
+let arr2 = ["i", "j", "o", "r"];
+console.log(arr2.reverse());
+// reverseđảo ngược trật tự
+//concat nối mảng
+//joinPhương thức array join() trong Javascript kết nối tất cả các phần tử trong một mảng thành một chuỗi.
+console.log(arr.concat(arr2));
+console.log(arr.join("-"));
