790c2ddffa3ad22866f5724bf6ffa03d6e9dfb72
Lee Thao <105211283+thaole2003@users.noreply.github.com>
2023-02-07 15:53:16 +0700

    Asynchronous JavaScript, AJAX and APIs

---
+++ S16/script.js
@@ -0,0 +1,41 @@
+//Callback trong lập trình là một hàm hoặc phương thức được truyền vào như là một tham số trong một hàm
+// hoặc phương thức khác, và được gọi sau khi hàm hoặc phương thức gốc hoàn thành.
+//  Callback thường được sử dụng để giải quyết việc tương tác giữa nhiều hàm hoặc phương thức trong một chương trình.
+// API (Application Programming Interface) trong lập trình là một tập hợp các giao diện, protocol và các quy tắc cho phép các phần mềm hoặc hệ thống tương tác với nhau. API cho phép một phần mềm gọi các dịch vụ, tính năng hoặc dữ liệu của một hệ thống khác mà không cần biết chi tiết về cách thức hoạt động của hệ thống đó. Ví dụ, một ứng dụng web có thể sử dụng API của một nền tảng để tải dữ liệu từ nền tảng đó và hiển thị cho người dùng.
+//API là 1 phần mềm có thể được sử dụng bởi phần mềm khác để cho phép các ứng dụng giao tiếp với nhau(dom api,geolocation api,own class api.online api)
+//api online :UD chạy trên máy chủ,nhận yêu cầu dữ liệu gửi laị dưới dạng phản hồi
+//JavaScript không đồng bộ là một kỹ thuật cho phép JavaScript thực hiện các tác vụ mà không cần chờ cho tác vụ trước hoàn thành. Khi một tác vụ không đồng bộ đang diễn ra, trình duyệt có thể tiếp tục thực hiện các tác vụ khác, chẳng hạn như xử lý sự kiện, và tác vụ đang diễn ra sẽ được hoàn thành sau đó. Điều này có thể giúp cho trình duyệt không bị treo khi phải chờ cho một tác vụ dài để hoàn thành, và giúp cho trải nghiệm người dùng tốt hơn.
+//Ajax (Asynchronous JavaScript and XML) là một kỹ thuật cho phép các ứng dụng web gửi và nhận dữ liệu từ máy chủ mà không cần tải lại trang web hoặc tải lại cả trang. Điều này có nghĩa là bạn có thể cập nhật một phần của trang mà không cần tải lại toàn bộ trang.
+
+// Ajax sử dụng JavaScript để gửi và nhận dữ liệu từ máy chủ mà không cần tải lại trang web. Nó sử dụng XMLHttpRequest để gửi và nhận dữ liệu mà không cần tải lại trang, hoặc sử dụng Fetch API. Ajax có thể sử dụng để tải dữ liệu từ máy chủ, cập nhật dữ liệu mà không cần tải lại trang, hoặc gửi dữ liệu đến máy chủ mà không cần tải lại trang.
+
+//
+const btn_country = document.querySelector(".btn_country");
+const country = document.querySelector(".country");
+const request = new XMLHttpRequest();
+const getcoutry = function (countryy) {
+  request.open("GET", `https://restcountries.com/v2/name/${countryy}`);
+  request.send();
+  console.log(request.responseText);
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+    <div class="country">
+    <img src="${data.flag}" alt="">
+    <div>
+        <h3>${data.name}</h3>
+        <h4>${data.region}</h4>
+        <p>${(data.population / 1000000).toFixed(1)}</p>
+        <p>${data.languages[0].name}</p>
+        <p>${data.currencies[0].name}</p>
+    </div>
+    </div>
+    `;
+    country.insertAdjacentHTML("beforeend", html);
+  });
+};
+getcoutry("portugal");
+
+// // getcoutry("germany");
+// getcoutry("usa");
